67d15321d8f883618660bc449711f9cc6345a50a
riceld1 <richard.eldridge@exfo.com>
2023-03-25 13:26:47 +0000

    broken

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import DateField from 'react-native-datefield';
 import logo from './images/logo.png';
 import './App.css';
 
@@ -6,10 +7,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       </header>
+        <p>Find out who is your fantasy premier league manager of the month!</p>
+     </header>
        <body className="App-body">
-        <GoButton />
-        </body>
+          <input id="leagueIdInput" type="number"/>
+          <p>Date</p>
+          <div>
+            <p>Rising star weighting</p>
+            <input id="risingStarId" type="number"/>
+            <p>Domination weighting</p>
+            <input id="dominationId" type="number"/>
+            <p>Consistency weighting</p>
+            <input id="consistencyId" type="number"/>
+            </div>
+          <GoButton />
+          <p>Manager of the month</p>
+          <Result />
+          </body>
         <div className="App-footer">
           <p>Copyright © 2023-present, Richard Eldridge. All rights reserved.</p>
         </div>
@@ -20,6 +34,12 @@ function App() {
 function GoButton() {
   return (
     <button className="simple-button">GO</button>
+  );
+}
+
+function Result() {
+  return (
+    <button className="simple-button">result</button>
   );
 }
 
