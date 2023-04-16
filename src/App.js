1fab1c1a903c30dfbe2390bfcace8659bf73fc1d
dridge <richard.eldridge2010@gmail.com>
2023-04-16 17:57:35 +0100

    Complete the basic UI with trivial functionality

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { useState } from 'react';
 
 export default function App() {
   const [leagueIdLive, setMessage] = useState('');
+  const [month, rising, domination, consistency] = useState('');
   const [updated, setUpdated] = useState(leagueIdLive);
   const handleClick = () => {
       // send inputs to be calculated
       setUpdated(leagueIdLive);
+      console.log(month);
   };
   const handleChange = (event) => {
       setMessage(event.target.value);
@@ -19,88 +21,95 @@ export default function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-header-text" >Who is your fantasy premier league manager of the month?</p>
      </header>
-       <body className="App-body">
-       <div class="row">
-         <div class="column left" style={{ color: `#aaa` }}>
-           <h2>Column 1</h2>
-           <p>Some text..</p>
-         </div>
-         <div class="column middle" style={{ color: `#bbb` }}>
-           <h2>Column 2</h2>
-           <p>Some text..</p>
-         </div>
-         <div class="column right" style= {{ color: `#ccc` }}>
-           <h2>Column 3</h2>
-           <p>Some text..</p>
-         </div>
-       </div>
-           <div className="row">
-              <div className="column left">
-                <label for="leagueId">League Id:</label>
-                <label for="month">Month:</label>
+     <body className="App-body responsive-three-column-grid">
+         <div></div>
+           <div className="responsive-three-column-grid">
+           <div>
+            <div>
+             <label>League Id:
+                    <input
+                      id="leagueId"
+                      name="leagueId"
+                      type="text"
+                      onChange={handleChange}
+                      value={leagueIdLive}/>
+              </label>
+            </div>
+           </div>
+               <div>
+                 <div>
+                 <label>Month:
+                    <select id="month" name="month">
+                      <option selected>January</option>
+                      <option>February</option>
+                      <option>March</option>
+                      <option>April</option>
+                      <option>May</option>
+                      <option>August</option>
+                      <option>September</option>
+                      <option>October</option>
+                      <option>November</option>
+                      <option>December</option>
+                    </select>
+                </label>
+                 </div>
+             </div>
+             <div></div>
+             <div>
+               <div>
+                <label> Rising star weighting:
+                    <input
+                        id="risingStarId"
+                        name="risingStar"
+                        type="range"
+                        min="0"
+                        max="10"
+                        step="1"
+                        />
+                </label>
+               </div>
+             </div>
+             <div>
+               <div>
+                  <label>Domination weighting:
+                      <input
+                        id="dominationId"
+                        name="domination"
+                        type="range"
+                        min="0"
+                        max="10"
+                        step="1"
+                        />
+                    </label>
+               </div>
+             </div>
+             <div>
+                <div>
+                  <label>Consistency weighting:
+                  <input
+                      id="consistencyId"
+                      name="consistency"
+                      type="range"
+                      min="0"
+                      max="10"
+                      step="1"
+                    />
+                    </label>
+                </div>
+             </div>
+             <div>
+             <button className="button" onClick={handleClick}>GO</button>
+             </div>
+              <div>
+                <p>Manager of the month:</p>
               </div>
-            </div>
-            <div className="column center"/>
-            <div className="column right">
-              <input
-                id="leagueId"
-                name="leagueId"
-                type="text"
-                onChange={handleChange}
-                value={leagueIdLive}/>
-
-              <select id="month" name="month">
-                <option selected value="01">January</option>
-                <option value="02">February</option>
-                <option value="03">March</option>
-                <option value="04">April</option>
-                <option value="05">May</option>
-                <option value="06">June</option>
-                <option value="07">July</option>
-                <option value="08">August</option>
-                <option value="09">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-              </select>
-            </div>
-          <br/><br/>
-          <label for="risingStarId">Rising star weighting:</label>
-            <input
-              id="risingStarId"
-              name="risingStar"
-              type="range"
-              value="5"
-              min="0"
-              max="10"
-              step="1"
-            />
-          <label for="dominationId">Domination weighting:</label>
-            <input
-              id="dominationId"
-              name="domination"
-              type="range"
-              value="5"
-              min="0"
-              max="10"
-              step="1"
-            />
-          <label for="consistencyId">Consistency weighting:</label>
-            <input
-              id="consistencyId"
-              name="consistency"
-              type="range"
-              value="5"
-              min="0"
-              max="10"
-              step="1"
-            />
-
-          <button className="simple-button" onClick={handleClick}>GO</button>
-          <p>Manager of the month</p>
-          <p className="simple-text">live result... {leagueIdLive}</p>
-          <p className="simple-text">go result... {updated}</p>
-          </body>
-       </div>
+              <div>
+                <p className="simple-text">live result... {leagueIdLive}</p>
+                <p className="simple-text">go result... {updated}</p>
+              </div>
+          </div>
+          <div></div>
+     </body>
+     </div>
   );
 }
